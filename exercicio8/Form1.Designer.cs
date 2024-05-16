3b5092ab77231088c3535efd3201d4adbc0f649b
henrique86h <henriqueribeiromiguel86@gmail.com>
2024-05-15 22:19:34 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio8/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace exercicio8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxIdade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxIdade
+            // 
+            this.textBoxIdade.BackColor = System.Drawing.Color.IndianRed;
+            this.textBoxIdade.Location = new System.Drawing.Point(113, 45);
+            this.textBoxIdade.Name = "textBoxIdade";
+            this.textBoxIdade.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIdade.TabIndex = 0;
+            this.textBoxIdade.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.CadetBlue;
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Informe sua Idade:";
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.BackColor = System.Drawing.SystemColors.Info;
+            this.btnValidar.Location = new System.Drawing.Point(87, 132);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(98, 35);
+            this.btnValidar.TabIndex = 2;
+            this.btnValidar.Text = "Validar";
+            this.btnValidar.UseVisualStyleBackColor = false;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(84, 96);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(277, 179);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxIdade);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxIdade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ exercicio8/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exercicio8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int idade = int.Parse(textBoxIdade.Text);
+
+                if (idade < 18)
+                {
+                    lblResultado.Text = "Sem permissão";
+                    lblResultado.ForeColor = Color.Red;
+                }
+                else
+                {
+                    lblResultado.Text = "Permissão concedida";
+                    lblResultado.ForeColor = Color.Green;
+                }
+            }
+            catch (FormatException)
+            {
+                lblResultado.Text = "Idade inválida";
+            }
+        }
+    }
+}
